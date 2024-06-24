37cc0c3f9161d3e4a9da9d66a914167289d5e070
Daniel Dangond <ddangond@ptc.com>
2024-06-24 14:53:00 -0400

    Added pvz and pvs to supported file types

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 const MAX_MESSAGE_SIZE = 300 * 1024 * 1024;
-const VALID_FILETYPES = ['css', 'csv', 'dat', 'fbx', 'gif', 'glb', 'htm', 'html', 'jpg', 'jpeg', 'js', 'json', 'map', 'mp4', 'obj', 'otf', 'pdf', 'ply', 'png', 'splat', 'svg', 'ttf', 'wasm', 'webm', 'webp', 'woff', 'xml', 'zip', '3dt'];
+const VALID_FILETYPES = ['css', 'csv', 'dat', 'fbx', 'gif', 'glb', 'htm', 'html', 'jpg', 'jpeg', 'js', 'json', 'map', 'mp4', 'obj', 'otf', 'pdf', 'ply', 'png', 'pvs', 'pvz', 'splat', 'svg', 'ttf', 'wasm', 'webm', 'webp', 'woff', 'xml', 'zip', '3dt'];
 /**
  * @typedef {'action' | 'beat' | 'delete' | 'get' | 'io' | 'keys' | 'message' | 'new' | 'patch' | 'ping' | 'post' | 'pub' | 'put' | 'res' | 'sub' | 'unsub' | 'meta'} MethodString
  */
